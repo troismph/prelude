2c5c07ac90450074a7c41f190b0c2fd805569f0f
g4z3 <g4z3g4z3@gmail.com>
2018-03-07 21:58:30 +0800

    Customizations on packages and faces

---
+++ modules/g4z3.el
@@ -0,0 +1,10 @@
+;; g4z3's custom packages
+
+;;; Code:
+
+(prelude-require-package 'neotree)
+(prelude-require-package 'virtualenvwrapper)
+(load-theme 'smart-mode-line-light)
+
+(provide 'g4z3)
+;;; g4z3.el ends here
---
+++ personal/g4z3_custom.el
@@ -0,0 +1,13 @@
+;;; Commentary:
+;;; g4z3's custom file, copy it to personal/custom.el
+
+;;; Code:
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common-selection ((t (:background "blue"))))
+ '(company-tooltip-selection ((t (:background "blue" :foreground "#DCDCCC")))))
+;;; g4z3_custom.el ends here
