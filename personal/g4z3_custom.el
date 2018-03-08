f7a86c9ee47cacc9ae084d70635d05dfdc006ed0
g4z3 <g4z3g4z3@gmail.com>
2018-03-08 10:05:04 +0800

    Customize faces

--- personal/g4z3_custom.el
+++ personal/g4z3_custom.el
@@ -6,11 +6,24 @@
 (global-linum-mode 1)
 (setq linum-format "%d ")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-tooltip ((t (:background "white" :foreground "black"))))
  '(company-tooltip-common-selection ((t (:background "blue"))))
- '(company-tooltip-selection ((t (:background "blue" :foreground "#DCDCCC")))))
+ '(company-tooltip-selection ((t (:background "blue" :foreground "#DCDCCC"))))
+ '(whitespace-tab ((t (:background "black" :underline t)))))
+
+(load-theme 'smart-mode-line-light)
 ;;; g4z3_custom.el ends here
