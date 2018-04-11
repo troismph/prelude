d969441784b10249467ae57afa7b4cd330a0296b
penghan <penghan@yitu-inc.com>
2018-04-11 10:54:01 +0800

    Personal settings

--- modules/g4z3.el
+++ modules/g4z3.el
@@ -6,5 +6,8 @@
 (prelude-require-package 'virtualenvwrapper)
 (prelude-require-package 'company-c-headers)
 
+(setq projectile-switch-project-action 'neotree-projectile-action)
+(global-set-key (kbd "M-x") 'execute-extended-command)
+
 (provide 'g4z3)
 ;;; g4z3.el ends here
